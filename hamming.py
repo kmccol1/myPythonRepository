ea23b2a66d1f3b92b7e9074c056fc83a6676cc36
kmccol1 <60723358+kmccol1@users.noreply.github.com>
2020-04-20 00:33:13 -0500

    Update hamming.py

--- hamming.py
+++ hamming.py
@@ -1,22 +1,24 @@
 #Kyle McColgan
 #19-April-2020
 #hamming.py
-#Hamming Single-bit Forward Error correction algorithm implementation in Python!
+#Python3 program implementation of Hamming single-bit forward
+#error correction algorithim.
+#Precondition: operates on 1 byte of data (8-bits).
 
 import re
 
+#******************************************************************************
+
 def calculateParity ( bits , flag ):
 	parity = 0
-	numOnes = 0
-
-	for bit in bits:
-		if bit == 1:
-			numOnes += 1
+	numOnes = bits.count(1)
 	
 	if (numOnes % 2 != 0) & (flag == 1):
 		parity = 1
 
 	return parity
+
+#******************************************************************************
 			
 def getHammingCode ( bits , flag ):
 	hamming = [2,2,0,2,0,0,0,2,0,0,0,0]
@@ -30,9 +32,9 @@ def getHammingCode ( bits , flag ):
 		hamIndex += 1
 	
 	p1 = [0,0,0,0,0,0]
-	p2 = [ hamming[1],hamming[2],hamming[5],hamming[6],hamming[9],hamming[10]]
+	p2 = [hamming[1],hamming[2],hamming[5],hamming[6],hamming[9],hamming [10]]
 	p4 = [hamming[3],hamming[4],hamming[5],hamming[6],hamming[11]]
-	p8 = [0,0,0,0,0]
+	p8 = [hamming[7],hamming[8],hamming[9],hamming[10],hamming[11]]
 
 	count = 0
 	pIndex = 0
@@ -42,11 +44,6 @@ def getHammingCode ( bits , flag ):
 			pIndex += 1
 		count += 1
 
-	count = 7
-	for val in p8:
-		val = hamming [ count ]
-		count += 1
-
 	hamming[0] = calculateParity ( p1 , flag )
 	hamming[1] = calculateParity ( p2, flag )
 	hamming[3] = calculateParity ( p4, flag )
@@ -54,9 +51,12 @@ def getHammingCode ( bits , flag ):
 			
 	return hamming
 
+#******************************************************************************
+
 def decodeHamming ( hamming , flag ):
 	decoded = [None] * 8
 	errorCode = [None] * 4
+	errorPos = 0
 
 	parity1 = [hamming[0], hamming[2], hamming[4], hamming[6], hamming[8], hamming[10] ]
 	parity2 = [hamming[1], hamming[2], hamming[5], hamming[6], hamming[9], hamming[10] ]
@@ -73,16 +73,9 @@ def decodeHamming ( hamming , flag ):
 	print ( "\nError code: ")
 	print ( errorCode )
 
-	errorPos = 0
+	errorPos = int( str(error4)+str(error3)+str(error2)+str(error1), 2)
+
 	if 1 in errorCode:
-		if error1 == 1:
-			errorPos += 1
-		elif error2 == 1:
-			errorPos += 2
-		elif error3 == 1:
-			errorPos += 4
-		elif error4 == 1:
-			errorPos += 8
 		print ( "\nError detected at element: " + str(errorPos) )
 		print ( "Correcting single-bit error..." )
 		
@@ -106,6 +99,8 @@ def decodeHamming ( hamming , flag ):
 
 	return decoded
 
+#******************************************************************************
+
 def getMsg ( ):
 	msg = '1'
 	print ( "\nEnter 8-bit data on single line: ")
@@ -114,27 +109,164 @@ def getMsg ( ):
 	while not re.match("\d{8}" , msg ):
 		msg = input ( "Please enter the 8-bit message: ")
 		if not re.match ("\d{8}" , msg ):
-			print ( "Error! Only 8-digit string required")
+			print ( "Error! Only 8-bit string allowed")
 
 	msg = [ int ( x ) for x in msg ]
 
 	return msg
 
+#******************************************************************************
 
+continueFlag = 'y'
 
-msg = getMsg ( )
-evenOddFlag = input ( "Even/odd parity bit? (e/o): " ) [0]
+while continueFlag == 'y':
+	msg = getMsg()
+	evenOddFlag = input("Even/odd parity bit? (e/o): ") [0]
 
-if evenOddFlag == 'e':
-	evenOddFlag = 1
-else:
-	evenOddFlag = 0
+	if evenOddFlag.lower() == 'e':
+		evenOddFlag = 1
+	else:
+		evenOddFlag = 0
 
-print ( "\nYou entered: " )
-print ( msg )
-print ( "\nhamming code: " )
-data = getHammingCode ( msg , evenOddFlag)
-print ( data )
-print ( decodeHamming ( data , evenOddFlag ) )
+	print("\nYou entered: ")
+	print(msg )
+	print("\nhamming code: ")
+	data = getHammingCode (msg , evenOddFlag)
+	print (data)
+	print (decodeHamming(data, evenOddFlag))
 
+	continueFlag = input("\nEnter another codeword? (y/n): ") [0]
+
+print ( "\nGoodbye!" )
+
+#******************************************************************************
+
+#Enter 8-bit data on single line: 
+#Sample input formatting: XXXXXXXX
+#Please enter the 8-bit message: 10100000
+#Even/odd parity bit? (e/o): e
+
+#You entered: 
+#[1, 0, 1, 0, 0, 0, 0, 0]
+
+#hamming code: 
+#[1, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0]
+
+#Error code: 
+#[0, 0, 0, 0]
+
+#No error detected. Decoding original message...
+
+#Original message: 
+#[1, 0, 1, 0, 0, 0, 0, 0]
+
+#Enter another codeword? (y/n): y
+
+#Enter 8-bit data on single line: 
+#Sample input formatting: XXXXXXXX
+#Please enter the 8-bit message: 01101010
+#Even/odd parity bit? (e/o): o
+
+#You entered: 
+#[0, 1, 1, 0, 1, 0, 1, 0]
+
+#hamming code: 
+#[0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0]
+
+#Error code: 
+#[0, 0, 0, 0]
+
+#No error detected. Decoding original message...
+
+#Original message: 
+#[0, 1, 1, 0, 1, 0, 1, 0]
+
+#Enter another codeword? (y/n): y
+
+#Enter 8-bit data on single line: 
+#Sample input formatting: XXXXXXXX
+#Please enter the 8-bit message: 10011010
+#Even/odd parity bit? (e/o): e
+
+#You entered: 
+#[1, 0, 0, 1, 1, 0, 1, 0]
+
+#hamming code: 
+#[0, 1, 1, 1, 0, 0, 1, 0, 1, 0, 1, 0]
+
+#Error code: 
+#[0, 0, 0, 0]
+
+#No error detected. Decoding original message...
+
+#Original message: 
+#[1, 0, 0, 1, 1, 0, 1, 0]
+
+#Enter another codeword? (y/n): y
+
+#Enter 8-bit data on single line: 
+#Sample input formatting: XXXXXXXX
+#Please enter the 8-bit message: 10101010
+#Even/odd parity bit? (e/o): e
+
+#You entered: 
+#[1, 0, 1, 0, 1, 0, 1, 0]
+
+#hamming code: 
+#[1, 1, 1, 1, 0, 1, 0, 0, 1, 0, 1, 0]
+
+#Error code: 
+#[0, 0, 0, 0]
+
+#No error detected. Decoding original message...
+
+#Original message: 
+#[1, 0, 1, 0, 1, 0, 1, 0]
+
+#Enter another codeword? (y/n): y
+
+#Enter 8-bit data on single line: 
+#Sample input formatting: XXXXXXXX
+#Please enter the 8-bit message: 10000100
+#Even/odd parity bit? (e/o): e
+
+#You entered: 
+#[1, 0, 0, 0, 0, 1, 0, 0]
+
+#hamming code: 
+#[1, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0]
+
+#Error code: 
+#[0, 0, 0, 0]
+
+#No error detected. Decoding original message...
+
+#Original message: 
+#[1, 0, 0, 0, 0, 1, 0, 0]
+
+#Enter another codeword? (y/n): y
+
+#Enter 8-bit data on single line: 
+#Sample input formatting: XXXXXXXX
+#Please enter the 8-bit message: 11001100
+#Even/odd parity bit? (e/o): e
+
+#You entered: 
+#[1, 1, 0, 0, 1, 1, 0, 0]
+
+#hamming code: 
+#[1, 0, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0]
+
+#Error code: 
+#[0, 1, 0, 0]
+
+#Error detected at element: 4
+#Correcting single-bit error...
+
+#Original message: 
+#[1, 1, 0, 0, 1, 1, 0, 0]
+
+#Enter another codeword? (y/n): n
+
+#Goodbye!
 
