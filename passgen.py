11038b13d08872fb90f297a498b830cd7e9f68da
kmccol1 <60723358+kmccol1@users.noreply.github.com>
2020-05-17 22:20:01 -0500

    Update passgen.py

--- passgen.py
+++ passgen.py
@@ -1,10 +1,13 @@
-#16-May-2020
+#17-May-2020
 #passgen.py
+#Cryptographic random data password generator
+#Requires Python 3.7.4
 
 #******************************************************************************
 
 import random
 import os
+import re
 
 #******************************************************************************
 
@@ -68,36 +71,28 @@ def getPassLength ( ):
 
 #******************************************************************************
 
-def getPassword ( validBits , passwordLength ):
-    code = []
-    bits = ''.join(list(validBits))
-
-    for i in range ( passwordLength ):
-        code  += str(''.join(bits[i + 2] )) 
-
-    return code
-
-#******************************************************************************
-
 def generatePassword ( passwordLength ):
-    #passwordLength = getPassLength ( )
-
     try:
         if passwordLength > 0:
             randomBits = str( os.urandom ( passwordLength ) )
     except NotImplementedError:
         print( 'Runtime error: "randomness source not found" ')
 
+    #generated bits example: "b'\x18\xe2.\xeeRR\x11\xd0b\x87'"
+
     #trim '\x' byte marker repetitions in random generated bits...
-    validBits =  list((filter ( lambda bit: bit != 'x' and bit != '\\' , randomBits ) ))
-    validBits = ''.join(validBits)
+    #validBits =  list((filter ( lambda bit: bit != 'x' and bit != '\\' , randomBits ) ))
+    validBits = str()
+
+    validBits += randomBits [5:len(randomBits)]
+    passcode = str( re.sub(r"\\x" , '', str(validBits) ) )
 
     #print ( "\nGenerated: ")
     #print ( randomBits )
     #print ( "\nValid:" )
     #print ( validBits )
 
-    passcode = ''.join( getPassword ( validBits , passwordLength ))
+    passcode = passcode[0:passwordLength]
 
     return passcode
 
