d767476057e2cc6fba229d05e70eed6c0835333d
kmccol1 <60723358+kmccol1@users.noreply.github.com>
2020-06-10 22:53:05 -0500

    Create testNetworkSpeeds.py

---
+++ testNetworkSpeeds.py
@@ -0,0 +1,21 @@
+#10-June-2020
+#mySpeedTester.py
+#A simple network transfer speed calculator.
+#Requires speedtest.py module
+#Requires Python 3.7.4
+
+import speedtest
+
+def calculateSpeeds ( ):
+    myTest = speedtest.Speedtest()
+    myTest.get_servers()
+    myTest.get_best_server()
+    myTest.download()
+    myTest.upload()
+    results = myTest.results.dict()
+    return results["download"], results["upload"], results["ping"]
+
+download , upload , ping = calculateSpeeds()
+print ( 'Testing speeds....')
+print('Download: ' + str(download))
+print('Upload: ' + str(upload))
