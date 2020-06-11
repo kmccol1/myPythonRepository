4b30dba963f65b30fc7da966225a298b1d31eeea
kmccol1 <60723358+kmccol1@users.noreply.github.com>
2020-06-11 00:00:23 -0500

    Update networkSpeed.py

--- networkSpeed.py
+++ networkSpeed.py
@@ -1,21 +1,23 @@
-#10-June-2020
+#11-June-2020
 #mySpeedTester.py
 #A simple network transfer speed calculator.
-#Requires speedtest.py module from https://pypi.org/project/speedtest-cli/
+#Requires speedtest.py module
 #Requires Python 3.7.4
 
 import speedtest
 
 def calculateSpeeds ( ):
-    myTest = speedtest.Speedtest()
-    myTest.get_servers()
-    myTest.get_best_server()
-    myTest.download()
-    myTest.upload()
-    results = myTest.results.dict()
-    return results["download"], results["upload"], results["ping"]
+    myTest = speedtest.Speedtest ( )
+    myTest . get_servers ( )
+    myTest . get_best_server ( )
+    myTest . download ( )
+    myTest . upload ( )
+    results = ( myTest.results ) . dict ( )
+    print ( results["download"] )
+    print ( results["upload"] )
+    print ( results["ping"] )
 
-download , upload , ping = calculateSpeeds()
-print ( 'Testing speeds....')
-print('Download: ' + str(download))
-print('Upload: ' + str(upload))
+calculateSpeeds ( )
+#print ( 'Testing speeds....')
+#print('Download: ' + str( (download / 2048).round() ) )
+#print('Upload: ' + str(upload / 2048))
