d59884ce141f9be2b58eb0b6f2cf15f651d7467e
kmccol1 <60723358+kmccol1@users.noreply.github.com>
2020-06-12 23:36:13 -0500

    Update networkSpeed.py

--- networkSpeed.py
+++ networkSpeed.py
@@ -1,23 +1,31 @@
-#11-June-2020
+#1-June-2020
 #mySpeedTester.py
-#A simple network transfer speed calculator.
+#A simple network transfer speed calculator script, set to execute every 5 minutes.
 #Requires speedtest.py module
 #Requires Python 3.7.4
 
 import speedtest
+import time
 
 def calculateSpeeds ( ):
-    myTest = speedtest.Speedtest ( )
-    myTest . get_servers ( )
-    myTest . get_best_server ( )
-    myTest . download ( )
-    myTest . upload ( )
+    myTest = speedtest.Speedtest()
+    myTest.get_servers()
+    myTest.get_best_server()
+    myTest.download()
+    myTest.upload()
     results = ( myTest.results ) . dict ( )
-    print ( results["download"] )
-    print ( results["upload"] )
-    print ( results["ping"] )
+    return results["download"] , results["upload"] , results ["ping"]
 
-calculateSpeeds ( )
-#print ( 'Testing speeds....')
-#print('Download: ' + str( (download / 2048).round() ) )
-#print('Upload: ' + str(upload / 2048))
+start = time.time()
+print ( 'Testing speeds....')
+count = 0
+with open ( 'speeds.txt' , 'w' ) as fout:
+    fout.write ( 'Download / Upload / Ping\n\n')
+    while True:
+        dl , ul , ping = calculateSpeeds ( )
+        fout.write ( "\nTest #" + str(count) + '\n')
+        fout.write ( str ( round( dl / 2048 )) + ' mbps \t /')
+        fout.write ( str ( round( ul / 2048 ) ) + ' mbps \t /')
+        fout.write ( str (round(ping)) + ' seconds')
+        fout.flush()
+        #time.sleep ( 300 - ((time.time() - start) % 300)
