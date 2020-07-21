a0cdb43d864b91f49b26c699f2c69dd3d5088ada
kmccol1 <60723358+kmccol1@users.noreply.github.com>
2020-07-21 00:02:21 -0500

    Update hostDiscovery.py

--- hostDiscovery.py
+++ hostDiscovery.py
@@ -1,79 +1,25 @@
-#*******************************************************************************************
-#This program mimicks basic functionality of the well-known iftop.  It reads network traffic 
-#and displays a table of current bandwidth usage by pairs of host addresses.
-#7-21-20
-#Requires Python 3.8.4, Scapy network API
-#*******************************************************************************************
+#A useful network reconaissance tool using ARP for host discovery.
+#7-20-20
+#Requires Python 3.8.4, Scapy API, Windows 10 OS. IPv4 only.
 
 from scapy.all import *
-from collections import Counter
-from os import system
 
-#*******************************************************************************************
+def displayActiveHosts ( ):
+    conf.verb = 0
+    ipAddressRange = "192.168.1.0/24"
+    activeHostList = list()
 
-captureLen = 30
-networkInterface = "Wi-Fi"
-packetCounter = Counter()
-hostDict = {}
+    arp = ARP(pdst= ipAddressRange )
+    ethernetFrame = Ether ( dst="ff:ff:ff:ff:ff:ff" )
+    arpPacket = ethernetFrame / arp
+    answersList = srp ( arpPacket, timeout = 3)[0]
 
-#*******************************************************************************************
+    for sent, received in answersList:
+        activeHostList.append({'ip': received.psrc, 'mac': received.hwsrc})
 
-def displayBandwith ( totalBytes ):
-    formattedBytes = str()
+    print ( "Active host list:")
+    print ( "IP" + " " * 18 + "MAC" )
+    for host in activeHostList:
+        print("{:16}    {}".format(host['ip'], host['mac'])) 
 
-    if totalBytes < 1024:
-        formattedBytes = "%3.2f Bytes" % ( totalBytes )
-    else:
-        totalBytes /= 1024
-        displayBandwith ( totalBytes )
-
-    return formattedBytes
-
-#*******************************************************************************************
-
-def filterPackets ( packet ):
-    if IP in packet:
-        packet = packet [ IP ]
-        packetCounter.update ( { tuple ( sorted ( map ( atol , ( packet.src, packet.dst)))): packet.len})
-
-#*******************************************************************************************
-
-def updatePacketCounter ( ):
-    sniff ( iface = networkInterface, prn = filterPackets, store = False,
-        timeout = captureLen )
-
-#*******************************************************************************************
-
-def printCommonHosts ( ):
-    for ( sourceAddr , destAddr ), bytesTransferred in packetCounter.most_common(10):
-        sourceAddr , destAddr = map ( ltoa , ( sourceAddr , destAddr ) )
-
-        for activeHost in ( sourceAddr , destAddr ):
-            if activeHost not in hostDict:
-                try:
-                    hostName = socket.gethostbyaddr(activeHost)
-                    hostDict [ activeHost ] = hostName [ 0 ]
-                except:
-                    hostDict [ activeHost ] = None
-
-        sourceAddr = "%s (%s)" % ( hostDict [ sourceAddr ] , sourceAddr ) if hostDict [ sourceAddr ] is not None else sourceAddr
-        destAddr = "%s (%s)" % ( hostDict[ destAddr ], destAddr ) if hostDict[ destAddr ] is not None else destAddr
-        bandwithFmt = displayBandwith( float ( bytesTransferred ) / captureLen )
-        print ( sourceAddr[0:20] , ' ' * ( 20 ), '=>' , destAddr[0:20] , ' ' * ( 20) , ' ' * 20 , bandwithFmt )
-        print ( ) 
-
-#*******************************************************************************************
-
-def printHeader ( ):
-    print ( '=' * 120)
-    print ( 'Host name' , ' ' * 33 , 'Destination Address' , ' ' * 33 , 'Total traffic ( bytes )' )
-    print ( '=' * 120)
-
-#*******************************************************************************************
-
-while True:
-    printHeader()
-    printCommonHosts()
-    updatePacketCounter()
-    os.system ( 'cls')
-
+displayActiveHosts()
