84242c3b54943c22c13622e395853e6fa1a31613
kmccol1 <60723358+kmccol1@users.noreply.github.com>
2020-07-21 00:02:41 -0500

    Update networkSniff.py

--- networkSniff.py
+++ networkSniff.py
@@ -1,16 +1,22 @@
-#Simple network sniffer using Scapy API
-#7-20-20
-#Requires Python 3.8.4, Scapy API, Windows 10 OS
+#*******************************************************************************************
+#This program mimicks basic functionality of the well-known iftop.  It reads network traffic 
+#and displays a table of current bandwidth usage by pairs of host addresses.
+#7-21-20
+#Requires Python 3.8.4, Scapy network API
+#*******************************************************************************************
 
 from scapy.all import *
 from collections import Counter
-import os
-import time
+from os import system
 
-captureLen = 10
+#*******************************************************************************************
+
+captureLen = 30
 networkInterface = "Wi-Fi"
 packetCounter = Counter()
 hostDict = {}
+
+#*******************************************************************************************
 
 def displayBandwith ( totalBytes ):
     formattedBytes = str()
@@ -23,15 +29,22 @@ def displayBandwith ( totalBytes ):
 
     return formattedBytes
 
+#*******************************************************************************************
+
 def filterPackets ( packet ):
     if IP in packet:
         packet = packet [ IP ]
         packetCounter.update ( { tuple ( sorted ( map ( atol , ( packet.src, packet.dst)))): packet.len})
 
-def getLiveOutput ( ):
+#*******************************************************************************************
+
+def updatePacketCounter ( ):
     sniff ( iface = networkInterface, prn = filterPackets, store = False,
         timeout = captureLen )
 
+#*******************************************************************************************
+
+def printCommonHosts ( ):
     for ( sourceAddr , destAddr ), bytesTransferred in packetCounter.most_common(10):
         sourceAddr , destAddr = map ( ltoa , ( sourceAddr , destAddr ) )
 
@@ -46,20 +59,21 @@ def getLiveOutput ( ):
         sourceAddr = "%s (%s)" % ( hostDict [ sourceAddr ] , sourceAddr ) if hostDict [ sourceAddr ] is not None else sourceAddr
         destAddr = "%s (%s)" % ( hostDict[ destAddr ], destAddr ) if hostDict[ destAddr ] is not None else destAddr
         bandwithFmt = displayBandwith( float ( bytesTransferred ) / captureLen )
-
-
-        print ( sourceAddr , ' ' * 30 , '=>' , destAddr , ' ' * 30 , bandwithFmt )
+        print ( sourceAddr[0:20] , ' ' * ( 20 ), '=>' , destAddr[0:20] , ' ' * ( 20) , ' ' * 20 , bandwithFmt )
         print ( ) 
 
+#*******************************************************************************************
 
+def printHeader ( ):
+    print ( '=' * 120)
+    print ( 'Host name' , ' ' * 33 , 'Destination Address' , ' ' * 33 , 'Total traffic ( bytes )' )
+    print ( '=' * 120)
 
-print ( 'Host name' , ' ' * 33 , 'Destination Address' , ' '* 33 , 'Total traffic ( bytes )' )
-print ( '=' * 120)
+#*******************************************************************************************
 
 while True:
-    getLiveOutput()
-    time.sleep(5)
-
-
-
+    printHeader()
+    printCommonHosts()
+    updatePacketCounter()
+    os.system ( 'cls')
 
