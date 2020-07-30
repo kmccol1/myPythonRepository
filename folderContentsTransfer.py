d65fcfdb4f45eb9bcb2ecc969b73056839fc357a
kmccol1 <60723358+kmccol1@users.noreply.github.com>
2020-07-30 00:00:29 -0500

    Update folderContentsTransfer.py

--- folderContentsTransfer.py
+++ folderContentsTransfer.py
@@ -16,13 +16,13 @@ from os.path import getmtime
 folderPath = "/home/user/Desktop/Source"
 destinationPath = "/home/user/Desktop/Destination"
 
-#filePath = folderPath + "/" + image
 imageList = [f for f in os.listdir ( folderPath ) if os.path.isfile ( os.path.join(folderPath , f ))]
 
 for image in imageList:
 	print (datetime.fromtimestamp(os.path.getmtime ( folderPath + "/" + image )).strftime("%m/%Y") )
 	folderName = (datetime.fromtimestamp(os.path.getctime ( folderPath + "/" + image)).strftime("%m:%Y"))
 	newPath = os.path.join ( destinationPath , folderName )
+	
 	if not os.path.exists ( newPath ):
 		os.makedirs ( newPath )
 		print ( "Directory " , newPath , " created ")
@@ -31,9 +31,6 @@ for image in imageList:
 
 	oldImagePath = os.path.join ( folderPath , image)
 	newImagePath = os.path.join ( newPath , image ) 
-
 	shutil.copy2 ( oldImagePath , newImagePath ) 
 
 #******************************************************************************
-
-
