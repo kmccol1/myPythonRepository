7f1a98da735891fbc3f796703f8f7cb808b9c7c2
kmccol1 <60723358+kmccol1@users.noreply.github.com>
2020-08-01 23:53:19 -0500

    Update hostDiscovery.py

--- hostDiscovery.py
+++ hostDiscovery.py
@@ -1,6 +1,6 @@
-#very basic network reconaissance tool using layer 2 for host discovery.
+#very basic network reconaissance tool using layer 2 broadcasting for host discovery.
 #7-20-20
-#Requires Python 3.8.5, Scapy API
+#Requires Python 3.8.5, Scapy library
 
 #******************************************************************************
 
@@ -10,21 +10,23 @@ from scapy.all import *
 
 def displayActiveHosts ( ):
     conf.verb = 0
-    ipAddressRange = "192.168.1.0/24"
+    ipAddressRange = "192.168.1.0/24" #CIDR notation
     activeHostList = list()
 
-    arp = ARP(pdst= ipAddressRange )
-    ethernetFrame = Ether ( dst="ff:ff:ff:ff:ff:ff" )
+    arp = ARP ( pdst= ipAddressRange )
+    ethernetFrame = Ether ( dst="ff:ff:ff:ff:ff:ff" ) #broadcast address
     arpPacket = ethernetFrame / arp
-    answersList = srp ( arpPacket, timeout = 3)[0]
+    
+    
+    responseList = srp ( arpPacket, timeout = 3)[0]
 
-    for sent, received in answersList:
-        activeHostList.append({'ip': received.psrc, 'mac': received.hwsrc})
+    for sentData, recvData in responseList:
+        activeHostList.append({'layer3Addr': recvData.psrc, 'layer2Addr': recvData.hwsrc})
 
     print ( "Active host list:")
-    print ( "IP" + " " * 18 + "MAC" )
+    print ( "IP address" + " " * 18 + "MAC" )
     for host in activeHostList:
-        print("{:16}    {}".format(host['ip'], host['mac'])) 
+        print("{:16}    {}".format(host['layer3Addr'], host['layer2Addr'])) 
         
 #******************************************************************************
 
